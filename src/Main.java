b9f5eaa0230d945c0ac3e0c96ff90f20b904b4ab
Boris <bazar505@mail.ru>
2018-11-10 12:46:22 +0300

    random number

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@
 public class Main {
 
     public static void main(String[] args) {
-       String str = "Hello World!";
-       System.out.println(str);
-        System.out.println("again");
+       String str = "Random number is: ";
+       System.out.print(str);
+       int rand = (int)(Math.random() * 10);
+        System.out.println(rand);
     }
 }

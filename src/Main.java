84e8320ee4f477308b6d0c75d91c906de12c6046
Boris <bazar505@mail.ru>
2018-11-10 12:19:16 +0300

    hello world_2

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
        String str = "Hello World!";
        System.out.println(str);
+        System.out.println("again");
     }
 }
